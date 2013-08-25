e30a81d5b3bb4e6d0edb3b952f4ef1c25a338108
Russell Smith <velsk0@gmail.com>
2013-08-24 18:13:22 -0700

    Solution for problem 17.

---
+++ Problem17.py
@@ -0,0 +1,79 @@
+word_lookup = {
+    0 : 'zero',
+    1 : 'one',
+    2 : 'two',
+    3 : 'three',
+    4 : 'four',
+    5 : 'five',
+    6 : 'six',
+    7 : 'seven',
+    8 : 'eight',
+    9 : 'nine',
+    10 : 'ten',
+    11 : 'eleven',
+    12 : 'twelve',
+    13 : 'thirteen',
+    14 : 'fourteen',
+    15 : 'fifteen',
+    16 : 'sixteen',
+    17 : 'seventeen',
+    18 : 'eighteen',
+    19 : 'nineteen',
+    20 : 'twenty',
+    30 : 'thirty',
+    40 : 'forty',
+    50 : 'fifty',
+    60 : 'sixty',
+    70 : 'seventy',
+    80 : 'eighty',
+    90 : 'ninety',
+    100 : 'hundred',
+    1000: 'thousand'
+}
+
+def number_to_string(number):
+    original_number = number
+    # Check for thousands
+    quotient = number // 1000
+    name = ''
+    
+    if quotient is not 0:
+        name += '%s %s ' % (word_lookup[quotient], word_lookup[1000])
+        number = number % 1000
+    
+    # Check for hundreds
+    quotient = number // 100
+    if quotient is not 0:
+        name += '%s %s ' % (word_lookup[quotient], word_lookup[100])
+        number = number % 100
+    
+    # Check for tens
+    
+    if number is not 0:
+        quotient = number // 10
+        
+        # If the original number was over 100 we add and per British usage
+        if original_number > 100:
+            name += 'and '
+        # Special case where 1-19
+        if 1 <= number <= 19:
+            name += '%s ' % word_lookup[number]
+            number = 0
+        else:
+            name += '%s ' % word_lookup[quotient*10]
+            number = number % 10
+    
+    # Check for ones
+    if number is not 0:
+        name += '%s ' % word_lookup[number]
+    
+    return name
+
+if __name__ == "__main__":
+    # Construct array of strings representing numbers from 1,1000 with white space removed.
+    numbers = [number_to_string(i).replace(" ","") for i in xrange(1, 1001, 1)]
+    
+    # Join array by empty string
+    number_str = ''.join(numbers)
+    
+    print 'The string length is %d characters' % len(number_str)
